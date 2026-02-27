4662299a44a14bb60d48f3f7ce3bd055eae3189e
jinyongp <dev.jinyongp@gmail.com>
2026-02-27 17:12:52 +0900

    style: enhance JSDoc comments for PopbillClientConfig properties

--- packages/popbill/src/client/types.ts
+++ packages/popbill/src/client/types.ts
@@ -8,41 +8,57 @@ export interface PopbillClientConfig {
 
   /**
    * 팝빌 테스트 연동 여부입니다.
+   *
+   * @default false
    */
   isTest?: boolean
 
   /**
    * 스태틱 IP 사용 여부입니다.
+   *
+   * @default false
    */
   useStaticIp?: boolean
 
   /**
    * GA IP 사용 여부입니다.
+   *
+   * @default false
    */
   useGaIp?: boolean
 
   /**
    * 로컬 시간 사용 여부입니다.
+   *
+   * @default true
    */
   useLocalTime?: boolean
 
   /**
    * IP 제한 사용 여부입니다.
+   *
+   * @default true
    */
   ipRestrictOnOff?: boolean
 
   /**
    * 요청 `Accept-Encoding` 헤더 값입니다.
+   *
+   * @default 'gzip'
    */
   acceptEncoding?: string | null
 
   /**
    * 요청 `Accept-Language` 헤더 값입니다.
+   *
+   * @default ko-KR
    */
   acceptLanguage?: string
 
   /**
    * 요청 타임아웃(ms)입니다.
+   *
+   * @default 180_000 (3분)
    */
   requestTimeoutMs?: number
 
